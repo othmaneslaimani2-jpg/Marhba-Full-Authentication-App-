b1340a4e195d8d99432a60d60f7a76f18f961071
othmaneslaimani2-jpg <othmaneslaimani2@gmail.com>
2026-07-28 16:37:42 +0100

    feat: add authentication routes for registration, login, and user retrieval

---
+++ backend/src/routes/auth.routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const { register, login, getMe } = require('../controllers/auth.controller');
+const validateRegister = require('../middlewares/validateRegister');
+const validateLogin = require('../middlewares/validateLogin');
+const authenticate = require('../middlewares/authenticate');
+
+router.post('register', validateRegister, register);
+router.post('/login', validateLogin, login);
+router.get('/me', authenticate, getMe);
+
+module.exports = router;
