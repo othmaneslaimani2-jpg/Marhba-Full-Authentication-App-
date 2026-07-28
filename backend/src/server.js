a62ae82e520fed158ab5b76210f755143f3399b8
othmaneslaimani2-jpg <othmaneslaimani2@gmail.com>
2026-07-28 11:50:34 +0100

    feat: implement database connection, user model, and API documentation

---
+++ backend/src/server.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const path = require('path');
+const { apiReference } = require('@scalar/express-api-reference');
+const sequelize = require('sequelize');
+
+
+const logger = require('./middlewares/logger');
+const errorHandler = require('./middlewares/errorHandler');
+const authRoutes = require('./routes/auth.routes');
+
+const app = express();
+
+app.use(express.json());
+app.use(logger);
+
+app.use('/docs', apiReference({
+    theme: 'purple',
+    spec: {
+        content: () => require(path.join(__dirname, 'docs', 'OpenApi.json')),
+    },
+})
+);
+
+
+app.use('/api/auth', authRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+sequelize.sync().then(() => {
+    console.log("Base de données connectée !")
+    app.listen(PORT, () => {
+        console.log(`🚀 Le serveur tourne sur http://localhost:${PORT}`);
+        console.log(`📚 Documentation API : http://localhost:${PORT}/docs`);
+    });
+}).catch((err) => {
+    console.error("Erreur DB :", err);
+});
